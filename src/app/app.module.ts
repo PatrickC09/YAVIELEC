393e31b56414f179f46f35e9bf70561e02b706ea
JeanOC <jac.ortega@yavirac.edu.ec>
2023-06-12 13:15:42 -0500

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/auth/login/login.component';
+import { LoginComponent } from './pages/auth/login/login/login.component';
+import { CandidatoComponent } from './pages/auth/login/candidato/candidato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CandidatoComponent
   ],
   imports: [
     BrowserModule,
